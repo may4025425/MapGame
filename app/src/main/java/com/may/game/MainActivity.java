12d427db3ba2016d326667949f92038ab278e509
may4025425 <may4025425@gmail.com>
2019-12-17 16:26:56 +0800

    12/17

---
+++ app/src/main/java/com/may/game/MainActivity.java
@@ -0,0 +1,55 @@
+package com.may.game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Button bLeft = findViewById(R.id.button_left);
+//        bLeft.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//  上面是方法一，implenents是方法二
+        findView();
+
+    }
+
+    private void findView() {
+        findViewById(R.id.button_left).setOnClickListener(this);
+        findViewById(R.id.button_right).setOnClickListener(this);
+        findViewById(R.id.button_up).setOnClickListener(this);
+        findViewById(R.id.button_down).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_up:
+                Log.d("MainActivity","OnClick:up");
+                break;
+            case R.id.button_down:
+                Log.d("MainActivity","OnClick:down");
+                break;
+            case R.id.button_left:
+                Log.d("MainActivity","OnClick:left");
+                break;
+            case R.id.button_right:
+                Log.d("MainActivity","OnClick:right");
+                break;
+        }
+
+    }
+}
